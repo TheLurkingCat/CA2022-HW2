dd36d2c567f68aba9a3512533073470114a40184
P.T.Liu <meow.cs07@nycu.edu.tw>
2022-04-01 01:47:43 +0800

    Add rotational metrics and fix some index

--- src/kinematics.cpp
+++ src/kinematics.cpp
@@ -96,40 +96,43 @@ Motion motionBlend(const Motion& motionA, const Motion& motionB) {
   //   3. A better one considered both translations and rotations.
   //   4. Your animation should smoothly change from motionA to motionB.
   //   5. You can adjust those `constexpr`s above by yourself if you need.
+  newMotion.posture().reserve(motionA.size() + motionB.size());
   const Posture& firstB = motionB.posture(0);
-  int start = motionA.size() - matchRange - blendFrameCount;
   int boneNum = static_cast<int>(motionA.posture(0).rotations.size());
+  int start = motionA.size() - matchRange - blendFrameCount;
+  // Unaffected motionA
+  for (int i = 0; i < start; ++i) newMotion.posture().emplace_back(motionA.posture(i));
+  // Motion matching
   for (int i = 0; i < matchRange; ++i) {
     for (int j = 1; j < boneNum; ++j) {
       difference[i] += (motionA.posture(start + i).translations[j] - firstB.translations[j]).norm();
+      difference[i] += 2 * std::acos(std::abs(motionA.posture(start + i).rotations[j].dot(firstB.rotations[j])));
     }
   }
   int blendStart = static_cast<int>(std::min_element(difference, difference + matchRange) - difference);
-  newMotion.posture().insert(newMotion.posture().end(), motionA.posture().begin(),
-                             motionA.posture().begin() + start + blendStart);
-
-  const Posture& firstA = motionA.posture(start + blendStart + 1);
+  // Unaffected motionA
+  for (int i = 0; i < blendStart; ++i) newMotion.posture().emplace_back(motionA.posture(start + i));
+  const Posture& firstA = motionA.posture(start + blendStart);
   Eigen::Vector3f rootTranslate = firstA.translations[0] - firstB.translations[0];
   Eigen::Quaternionf rootRotation = firstB.rotations[0].inverse() * firstA.rotations[0];
-
+  // Blending
   for (int i = 0; i < blendFrameCount; ++i) {
     Posture& temp = newMotion.posture().emplace_back(boneNum);
     float currentFactor = 0.5f + std::sin(EIGEN_PI * blendFactor * i - EIGEN_PI / 2) / 2;
     // root
-    temp.translations[0] = (1 - currentFactor) * motionA.posture(start + blendStart + i + 1).translations[0] +
+    temp.translations[0] = (1 - currentFactor) * motionA.posture(start + blendStart + i).translations[0] +
                            currentFactor * (motionB.posture(i).translations[0] + rootTranslate);
-    temp.rotations[0] = motionA.posture(start + blendStart + i + 1)
+    temp.rotations[0] = motionA.posture(start + blendStart + i)
                             .rotations[0]
                             .slerp(currentFactor, motionB.posture(i).rotations[0] * rootRotation);
     for (int j = 1; j < boneNum; ++j) {
-      temp.translations[j] = (1 - currentFactor) * motionA.posture(start + blendStart + i + 1).translations[j] +
+      temp.translations[j] = (1 - currentFactor) * motionA.posture(start + blendStart + i).translations[j] +
                              currentFactor * motionB.posture(i).translations[j];
-      temp.rotations[j] = motionA.posture(start + blendStart + i + 1)
-                              .rotations[j]
-                              .slerp(currentFactor, motionB.posture(i).rotations[j]);
+      temp.rotations[j] =
+          motionA.posture(start + blendStart + i).rotations[j].slerp(currentFactor, motionB.posture(i).rotations[j]);
     }
   }
-
+  // Unaffected motionB
   for (int i = blendFrameCount; i < motionB.size(); ++i) {
     Posture& temp = newMotion.posture().emplace_back(motionB.posture(i));
     temp.translations[0] = motionB.posture(i).translations[0] + rootTranslate;
